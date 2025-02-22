28242d3c6857282f7717e2cd674d5248d4eab1a1
Isabelle <isasoarez21@gmail.com>
2025-02-22 11:32:50 -0300

    feat: add Button and IconButton component

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps, ReactNode } from 'react'
+interface IconButtonProps extends ComponentProps<'button'> {
+  children: ReactNode
+}
+
+export function IconButton(props: IconButtonProps) {
+  return (
+    // biome-ignore lint/a11y/useButtonType: <explanation>
+    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
+      {props.children}
+    </button>
+  )
+}
