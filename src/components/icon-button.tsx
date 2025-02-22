b7c86c7b840e44f5b143bc8d73836dd957974a6f
Isabelle <isasoarez21@gmail.com>
2025-02-22 18:44:40 -0300

    fix: render link and icon in inputField and IconButton

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,13 +1,16 @@
-import type { ComponentProps, ReactNode } from 'react'
-interface IconButtonProps extends ComponentProps<'button'> {
-  children: ReactNode
-}
+import { type ComponentProps, ReactNode } from 'react'
 
-export function IconButton(props: IconButtonProps) {
-  return (
-    // biome-ignore lint/a11y/useButtonType: <explanation>
-    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-      {props.children}
-    </button>
-  )
+import { twMerge } from 'tailwind-merge'
+interface IconButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton({ className, ...props }: IconButtonProps) {
+    return (
+        <button
+            className={twMerge(
+                'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+                className
+            )}
+            {...props}
+        />
+    )
 }
